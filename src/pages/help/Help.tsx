72e584de9bd251952f5001f6558c2790272794e6
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-30 18:26:39 -0400

    feat: help page

--- src/pages/help/Help.tsx
+++ src/pages/help/Help.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
+import styles from './Help.module.css'
+import Accordion from '../../components/Accordion/Accordion';
+
+interface Question {
+    question: string;
+    answer: string;
+}
+
 function Help () {
+    const [questions, setQuestions] = useState<Question[]>([]);
+    useEffect(() => {
+        getQuestions();
+    }, [])
+
+    const getQuestions = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:5000/api/help');
+            const data: Question[] = await response.json();
+            setQuestions(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
-        Help!
+        <h1 className='text-center'>Frequently asked questions</h1>
+        <section className={styles['questions-container']}>
+            {questions.map((question, index) =>
+                <Accordion summary={question.question} details={question.answer} key={`${question.question}-${index}`} />
+            )}
+        </section>
         </>
     )
 }

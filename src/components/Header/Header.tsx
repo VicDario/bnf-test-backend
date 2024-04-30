0cafef93812d9824e8a37312792070f8a90ffd3d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-30 15:49:28 -0400

    feat: improve styles and add icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import logo from '../../assets/slack_logo.png';
 import styles from './Header.module.css';
+import { FaChevronDown, FaSearch } from "react-icons/fa";
 
 function Header () {
     return (
@@ -8,16 +9,17 @@ function Header () {
                 <img src={logo} alt="Slack" className={styles.logo} />
                 <nav className={styles.navbar}>
                     <ul>
-                        <li>Features</li>
-                        <li>Solutions</li>
+                        <li>Features <FaChevronDown /></li>
+                        <li>Solutions <FaChevronDown /></li>
                         <li>Enterprise</li>
-                        <li>Resources</li>
+                        <li>Resources <FaChevronDown /></li>
                         <li>Pricing</li>
                     </ul>    
                 </nav>
             </div>
             <div className={styles.right}>
-                <a href="#" className='button text-black'>Sign in</a>
+                <span><FaSearch /></span>
+                <a href="#" className='text-black text-without-decoration'>Sign in</a>
                 <a href="#" className='button button-white'>Talk To Sales</a>
                 <a href="#" className='button button-purple'>Try for free</a>
             </div>
